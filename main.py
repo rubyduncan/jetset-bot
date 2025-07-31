f0c0f99acfb5ec1cf9165d8b142db6be0fbd0b32
rubyduncan <62487668+rubyduncan@users.noreply.github.com>
2025-07-31 09:56:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     # SLACK_CHANNEL = os.getenv("SLACK_CHANNEL", "#can-i-get-a-paper")
     SLACK_CHANNEL = os.getenv("SLACK_CHANNEL", "#arxiv_bot_test")
     
-   INCLUDE_TERMS_LIST = ["black hole", "AGN", "jet", "jet model", "neutrinos", "neutrino", "microquasar", "active galactic nuclei", "X-ray binary", "XRB", "particle acceleration", "cosmic rays", "accretion", "GRMHD"]
+    INCLUDE_TERMS_LIST = ["black hole", "AGN", "jet", "jet model", "neutrinos", "neutrino", "microquasar", "active galactic nuclei", "X-ray binary", "XRB", "particle acceleration", "cosmic rays", "accretion", "GRMHD"]
 
     EXCLUDE_TERMS_LIST = ["exoplanet", "main sequence", "pulsar", "neutron star", "Earth", "planet", "comet", "martian", "supernovae", "tidal disruption event", "supernova", "pre–stellar","asteroid", "Voigt", "FRB", "Fast radio burst", "galaxy evolution", "Earth"]
 
